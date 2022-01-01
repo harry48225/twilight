302265df8692c98064bfaa341b17eed943b217f8
Harry Best <harrybest800@gmail.com>
2022-01-01 22:01:55 +0000

    Add microdot webserver

--- firmware/main.py
+++ firmware/main.py
@@ -2,6 +2,7 @@ import network
 import credentials
 from machine import Pin
 from time import sleep
+from microdot import Microdot, Response
 
 HOSTNAME = "twilight"
 
@@ -17,5 +18,15 @@ def get_wlan():
   print("connected!")
   return wlan
 
+app = Microdot()
+
+@app.route('/')
+def index(request):
+  return Response(body="hi")
+
+
 motor = Pin(32, Pin.OUT)
 wlan = get_wlan()
+app.run(debug=True, port=80)
+print("hi from after the run")
+
