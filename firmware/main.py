6217fd9e62fea757fbd583f0a865ecb2b84e5afb
Harry Best <harrybest800@gmail.com>
2022-01-01 21:30:43 +0000

    implement basic network connection

--- firmware/main.py
+++ firmware/main.py
@@ -1,9 +1,21 @@
+import network
+import credentials
 from machine import Pin
 from time import sleep
-motor = Pin(32, Pin.OUT)
 
-while True:
-  motor.on()
-  motor.off()
-  sleep(0.001)
-  print("rotate")
+HOSTNAME = "twilight"
+
+def get_wlan():
+  wlan = network.WLAN(network.STA_IF)
+  wlan.active(True)
+  wlan.config(dhcp_hostname=HOSTNAME)
+  wlan.connect(credentials.SSID, credentials.PASSWORD)
+
+  print(f"connecting to wlan {credentials.SSID}, with password: {credentials.PASSWORD}")
+  while not wlan.isconnected():
+    pass
+  print("connected!")
+  return wlan
+
+motor = Pin(32, Pin.OUT)
+wlan = get_wlan()
