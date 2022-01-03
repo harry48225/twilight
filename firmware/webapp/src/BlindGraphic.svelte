e3d7fe15239c3e7099ceb214798aed8abd8733ec
Harry Best <harrybest800@gmail.com>
2022-01-03 22:42:42 +0000

    Add windowsill

--- firmware/webapp/src/BlindGraphic.svelte
+++ firmware/webapp/src/BlindGraphic.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+  export let height: number = 0;
 
+  setTimeout(() => {height = 256}, 500);
 </script>
 
 <div class="canvas">
-  <div class="blind">
+  <div class="blind" style={`height: ${height}px`}>
   </div>
 </div>
 
@@ -22,15 +24,29 @@
     width: 256px;
     border: 2px #444 solid;
     transform-style: preserve-3d;
+    display: block;
     transform: skewY(30deg);
+    background-color: #222;
 
-    &::after {
+    // Top of blind
+    &::before {
       content: "";
       display: inline-block;
       height: 32px;
       width: 256px;
       border: 2px #444 solid;
       transform: skewX(-45deg) translateY(-36px) translateX(-21px);
+    }
+
+    // Window sill
+    &::after {
+      content:"";
+      display: block;
+      border: 2px #c8c8c8 solid;
+      width: 320px;
+      height: 64px;
+      transform: skewX(-45deg) translateY(440px) translateX(408px) translateZ(-1px);
+      
     }
   }
 </style>
