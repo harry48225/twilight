5beee90c50e183f15d0c36baf4b5161a526f2583
Harry Best <harrybest800@gmail.com>
2022-03-27 20:48:29 +0100

    Add watchdog timer

--- firmware/main.py
+++ firmware/main.py
@@ -1,12 +1,13 @@
 import network
 import credentials
-from machine import freq
+from machine import freq, Pin
 from time import sleep, localtime
 from microdot_asyncio import Microdot, Response, send_file
 import uasyncio
 import ntptime
 import suncalc
 from motor import Motor_Controller
+from machine import WDT
 
 LAT = 53.576866
 LONG = -2.428219
@@ -20,11 +21,15 @@ def get_wlan():
   wlan.connect(credentials.SSID, credentials.PASSWORD)
 
   print(f"connecting to wlan {credentials.SSID}, with password: {credentials.PASSWORD}")
+  status_LED.off()
   while not wlan.isconnected():
     print(".", end="")
-    sleep(1)
-    pass
+    status_LED.on()
+    sleep(0.5)
+    status_LED.off()
+    sleep(0.5)
   print("connected!")
+  status_LED.on()
   return wlan
 
 async def set_clock():
@@ -37,6 +42,18 @@ async def set_clock():
     except:
       print("error aquiring time, retrying")
       await uasyncio.sleep(1)
+
+async def feed_watchdog():
+  while True:
+    status_LED.off()
+    if wlan.isconnected():
+      wdt.feed()
+      await uasyncio.sleep_ms(100)
+      status_LED.on()
+      
+    await uasyncio.sleep(2)
+
+    
 
 app = Microdot()
 
@@ -86,8 +103,12 @@ async def sun_times(request):
 
 motor = Motor_Controller()
 
+status_LED = Pin(26, Pin.OUT)
+
 freq(240_000_000) # Set the clock speed to maximum
 wlan = get_wlan()
+wdt = WDT(timeout=5000) # Watchdog with timeout of 5 seconds
 uasyncio.create_task(set_clock())
+uasyncio.create_task(feed_watchdog())
 uasyncio.create_task(app.start_server(debug=True, port=80))
 uasyncio.get_event_loop().run_forever()
